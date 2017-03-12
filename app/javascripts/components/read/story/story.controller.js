adcad27bd371e1ecda49daf728cb64f27b21d0af
Steve Creswick <screswick@gmail.com>
2017-03-12 17:23:00 -0400

    Add ability to choose user inputs through click and keyup

--- app/javascripts/components/read/story/story.controller.js
+++ app/javascripts/components/read/story/story.controller.js
@@ -1,8 +1,8 @@
 export default angular.module( 'campfire' ).controller(
   'StoryController',
   [
-    '$scope', 'StoryFeed', '$routeParams',
-    function( $scope, StoryFeed, $routeParams ) {
+    '$scope', 'StoryFeed', '$routeParams', '$filter', '$document',
+    function( $scope, StoryFeed, $routeParams, $filter, $document ) {
 
       StoryFeed.getStory( $routeParams[ 'story_id' ] ).then(
         function( success ) {
@@ -10,16 +10,40 @@ export default angular.module( 'campfire' ).controller(
 
           if ( $scope.story ) {
             $scope.plotDeviceDisplayed = [];
-
             $scope.plotDeviceDisplayed.push( $scope.story.plot_devices[ 0 ] );
           }
         },
         function( error ) {
-          console.log(error);
+          console.log( error );
         }
       );
 
+      $scope.chooseInput = function( userInput ) {
+        var filterOptions = {
+          arrayToFilter: $scope.story.plot_devices,
+          idToFind: userInput.success_id
+        };
 
+        var nextDevice = $filter( 'findById' )( filterOptions );
+
+        if ( nextDevice ) {
+          $scope.plotDeviceDisplayed.push( nextDevice );
+        }
+        else {
+          console.log( 'The next text could not be found.' );
+        }
+      };
+
+      $document.on( 'keyup', function( event ) {
+        var lastIndex   =   $scope.plotDeviceDisplayed.length - 1,
+            plotDevice  =   $scope.plotDeviceDisplayed[ lastIndex ],
+            inputIndex  =   parseInt( event.key ) - 1;
+
+        if ( plotDevice.user_inputs[ inputIndex ] ) {
+          $scope.chooseInput( plotDevice.user_inputs[ inputIndex ] );
+          $scope.$apply();
+        }
+      } );
     }
   ]
 );

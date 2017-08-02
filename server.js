d6ef96191455204343559edef070026737c0838c
Steve Creswick <screswick@gmail.com>
2017-08-01 20:56:23 -0400

    Fix build path

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var path = require('path');
 var express = require('express');
 
+var BUILD_DIR = path.join( __dirname, 'build' );
+
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(BUILD_DIR));
 app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
+
+//Send index.html when the user access the web
+app.get( '*', function ( req, res ) {
+  res.sendFile( path.join( BUILD_DIR, 'index.html' ) );
+} );

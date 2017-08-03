1c55ff0406f1344dd8fce726bd23996488a834eb
Steve Creswick <screswick@gmail.com>
2017-08-02 22:44:37 -0400

    Add post request for environment info

---
+++ environments.js
@@ -0,0 +1,8 @@
+module.exports = {
+  production: {
+    apiUrl: '//adven-turing-machine.herokuapp.com'
+  },
+  staging: {
+    apiUrl: null
+  }
+};
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var path = require('path');
 var express = require('express');
 
+var environments = require( './environments' );
+var minimist = require( 'minimist' );
+
+var options = minimist( process.argv.slice( 2 ) );
+
 var BUILD_DIR = path.join( __dirname, 'build' );
+
+var environment = options.environment || options.e || process.env.NODE_ENV || 'staging';
+
+var currentEnvironment = environments[ environment ];
+
+console.log(currentEnvironment);
 
 var app = express();
 
@@ -12,7 +23,13 @@ var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
 
+app.post( '/environment', function( request, response ) {
+  response.set( 'Cache-Control', 'no-cache' );
+  response.send( ENV_OBJECT );
+} );
+
 //Send index.html when the user access the web
+
 app.get( '*', function ( req, res ) {
   res.sendFile( path.join( BUILD_DIR, 'index.html' ) );
 } );

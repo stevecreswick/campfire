d58316f479d82d139b15fa4ac92ea0afd9de0ee5
Steve Creswick <screswick@gmail.com>
2017-09-19 23:33:37 -0400

    Add edit if it is a user story

--- app/javascripts/components/read/story/story.controller.js
+++ app/javascripts/components/read/story/story.controller.js
@@ -1,17 +1,33 @@
 export default angular.module( 'campfire' ).controller(
   'StoryController',
   [
-    '$scope', 'StoryFeed', '$routeParams', '$filter', '$document', '$attrs', 'Operation',
-    function( $scope, StoryFeed, $routeParams, $filter, $document, $attrs, Operation ) {
+    '$scope', '$rootScope', 'StoryFeed', '$routeParams', '$filter',
+    '$document', '$attrs', 'Operation',
+    function( $scope, $rootScope, StoryFeed, $routeParams, $filter,
+              $document, $attrs, Operation
+    ) {
       const STORY_URL = '/stories/' + $routeParams[ 'story_id' ];
+      $scope.userStory = false;
+
+      var checkUserStory = function() {
+        var currentUserId   =   $rootScope.currentUser.id,
+            storyUserId     =   $scope.story.author.id;
+
+
+
+        currentUserId === storyUserId ?
+          $scope.userStory = true :
+          $scope.userStory = false;
+      };
 
       Operation.query( STORY_URL ).then(
         function( response ) {
           $scope.story = response.data;
-
+          console.log(response);
           if ( $scope.story ) {
             $scope.plotDeviceDisplayed = [];
             $scope.plotDeviceDisplayed.push( $scope.story.plot_devices[ 0 ] );
+            checkUserStory();
           }
         },
         function( reason ) {
